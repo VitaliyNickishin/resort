b11f7df59c8913e23f243e0ab09b305c0270d362
“VitaliyNickishin” <nickishin34@gmail.com>
2019-06-26 19:15:27 +0300

    creating loading and room components

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactLoading from 'react-loading'
+
+const Example = ({type, color}) => (
+ <ReactLoading 
+  type={type}
+  color={color}
+  height={667}
+  width={375}
+ />
+);
+
+export default Example
--- src/context.js
+++ src/context.js
@@ -17,6 +17,29 @@ class RoomProvider extends Component {
  };
 //get Data
 
+ componentDidMount() {
+  // this.getData
+  let rooms = this.formatData(items);
+  //console.log(rooms);
+  let featuredRooms = rooms.filter(room => room.featured === true);
+  this.setState({
+   rooms, 
+   featuredRooms, 
+   sortedRooms: rooms, 
+   loading: false
+  })
+ }
+
+ formatData(items) {
+  let tempItems = items.map(item => {
+   let id = item.sys.id
+   let images = item.fields.images.map(image => image.fields.file.url);
+
+   let room = {...item.fields,images,id}
+   return room;
+  });
+  return tempItems
+ }
 
  static propTypes = {
   prop: PropTypes
