ceab8549474d3e5c6f41757c0fdf2b42b998d78f
“VitaliyNickishin” <nickishin34@gmail.com>
2019-06-28 18:29:11 +0300

    page singleroom is ready

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -28,13 +28,31 @@ export default class SingleRoom extends Component {
    return (
     <div className="error">
      <h3>no such room could be found</h3>
+     <Link to='/rooms' className='btn-primary'>
+      back to rooms
+     </Link>
     </div>
    )
   }
+  const {
+   name, 
+   description,
+   capacity,
+   size,
+   price, 
+   extras, 
+   breakfast, 
+   pets, 
+   images
+  } = room;
   return (
-   <div>
-    hello from single room page
-   </div>
+   <Hero hero='roomsHero'>
+    <Banner title={`${name} room`}>
+     <Link to='/rooms' className='btn-primary'>
+      back to room
+     </Link>
+    </Banner>
+   </Hero>
   )
  }
 }
