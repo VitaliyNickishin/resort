e2289122b56e6baf5dbbb63c7fc07724b9218e02
“VitaliyNickishin” <nickishin34@gmail.com>
2019-07-02 15:14:09 +0300

    SingleRoom page is completed

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -49,7 +49,7 @@ export default class SingleRoom extends Component {
   } = room;
 
   const [mainImg, ...defaultImg] = images
-  console.log(defaultImg);
+  //console.log(defaultImg);
   
 
   return (
@@ -62,11 +62,41 @@ export default class SingleRoom extends Component {
     </Banner>
    </StyledHero>
    <section className="single-room">
+
     <div className="single-room-images">
-     {images.map((item, index) => {
+     {defaultImg.map((item, index) => {
       return <img key={index} src={item} alt={name}/>
      })}
     </div>
+
+    <div className="single-room-info">
+     <article className="desc">
+      <h3>details</h3>
+      <p>{description}</p>
+     </article>
+     <article className="info">
+      <h3>info</h3>
+      <h6>price : ${price}</h6>
+      <h6>size : {size} SQFT</h6>
+      <h6>
+       max capacity : {
+        capacity > 1 ? `${capacity} people` : `${capacity} person`
+       }
+      </h6>
+      <h6>{pets ? "pets allowed" : "no pets alowed"}</h6>
+      <h6>{breakfast && "free breackfast included"}</h6>
+     </article>
+    </div>
+
+   </section>
+
+   <section className="room-extras">
+    <h6>extras</h6>
+    <ul className="extras">
+     {extras.map((item, index) => {
+      return <li key={index}>- {item}</li>
+     })}
+    </ul>
    </section>
    </>
   )
