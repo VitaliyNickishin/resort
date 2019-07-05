3e735072fb8df2ba1c1db0a3ad97f8f910e81815
“VitaliyNickishin” <nickishin34@gmail.com>
2019-07-05 16:53:13 +0300

    work with form in RoomFilter

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import {useContext} from 'react'
+import {RoomContext} from '../context'
+import Title from '../components/Title'
+
+//get all unique values
+const getUnique = (items, value) => {
+ 
+}
 
 export default function RoomFilter() {
+ const context = useContext(RoomContext);
+ //console.log(context);
+ const {
+  handleChange,
+  type,
+  capacity,
+  price,
+  minPrice,
+  maxPrice,
+  minSize,
+  maxSize,
+  breakfast,
+  pets
+ } = context;
+ 
  return (
-  <div>
-   hello from roomfilter
-  </div>
+  <section className="filter-container">
+   <Title title="search rooms" />
+   <form action="" className="filter-form">
+    {/* select type */}
+    <div className="form-group">
+     <label htmlFor="type">room type</label>
+     <select 
+      name = "type" 
+      id = "type" 
+      value = {type}
+      className = "form-control"
+      onChange = {handleChange}
+     >
+      <option value="single">single</option>
+      <option value="double">double</option>
+     </select>
+    </div>
+    {/* end select type */}
+   </form>
+  </section>
  )
 }
--- src/context.js
+++ src/context.js
@@ -13,8 +13,16 @@ class RoomProvider extends Component {
   rooms: [],
   sortedRooms: [],
   featuredRooms: [],
-  loading: true
-  
+  loading: true,
+  type: 'all',
+  capacity: 1,
+  price: 0,
+  minPrice: 0,
+  maxPrice: 0,
+  minSize: 0,
+  maxSize: 0,
+  breakfast: false,
+  pets: false
  };
 //get Data
 
@@ -23,11 +31,16 @@ class RoomProvider extends Component {
   let rooms = this.formatData(items);
   //console.log(rooms);
   let featuredRooms = rooms.filter(room => room.featured === true);
+  let maxPrice = Math.max(...rooms.map(item => item.price));
+  let maxSize = Math.max(...rooms.map(item => item.size));
   this.setState({
    rooms, 
    featuredRooms, 
    sortedRooms: rooms, 
-   loading: false 
+   loading: false,
+   price: maxPrice,
+   maxPrice,
+   maxSize
    //loading: true //появится загрузка
   })
  }
@@ -43,10 +56,23 @@ class RoomProvider extends Component {
   return tempItems
  }
 
- getRoom = (slug) => {
+ getRoom = slug => {
   let tempRooms = [...this.state.rooms];
   const room = tempRooms.find(room => room.slug === slug);
   return room;
+ };
+
+ handleChange = event => {
+  const type = event.target.type
+  const name = event.target.name
+  const value = event.target.value
+  console.log(type, name, value);
+  
+ }
+
+ filterRooms = () => {
+  console.log("hello");
+  
  }
 
  static propTypes = {
@@ -55,8 +81,13 @@ class RoomProvider extends Component {
 
  render() {
   return (
-   <RoomContext.Provider value = {{...this.state,
-    getRoom: this.getRoom}}>
+   <RoomContext.Provider 
+    value = {{
+     ...this.state,
+     getRoom: this.getRoom,
+     handleChange: this.handleChange
+     }}
+   >
     {this.props.children}
    </RoomContext.Provider>
   )
